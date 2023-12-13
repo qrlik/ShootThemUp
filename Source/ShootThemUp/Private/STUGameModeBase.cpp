62f678026801c252ea5bc697d29d74a47a046b73
Maksim Isakov <maksim.isakov@mytona.com>
2023-12-13 22:27:10 +0400

    lesson 23

--- Source/ShootThemUp/Private/STUGameModeBase.cpp
+++ Source/ShootThemUp/Private/STUGameModeBase.cpp
@@ -2,4 +2,10 @@
 
 
 #include "STUGameModeBase.h"
+#include "Player/STUBaseCharacter.h"
+#include "Player/STUPlayerController.h"
 
+ASTUGameModeBase::ASTUGameModeBase() {
+	DefaultPawnClass = ASTUBaseCharacter::StaticClass();
+	PlayerControllerClass = ASTUPlayerController::StaticClass();
+}
--- Source/ShootThemUp/Public/STUGameModeBase.h
+++ Source/ShootThemUp/Public/STUGameModeBase.h
@@ -13,5 +13,7 @@ UCLASS()
 class SHOOTTHEMUP_API ASTUGameModeBase : public AGameModeBase
 {
 	GENERATED_BODY()
-	
+
+public:
+	ASTUGameModeBase();
 };
