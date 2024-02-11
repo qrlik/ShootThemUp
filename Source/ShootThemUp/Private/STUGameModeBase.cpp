85772e2f33dc5b47601cb78f8a2692235df9f469
Maksim Isakov <qrlikk@gmail.com>
2024-02-11 21:05:13 +0400

    lesson 111

--- Source/ShootThemUp/Private/STUGameModeBase.cpp
+++ Source/ShootThemUp/Private/STUGameModeBase.cpp
@@ -2,6 +2,7 @@
 
 #include "STUGameModeBase.h"
 
+#include "AIController.h"
 #include "Player/STUBaseCharacter.h"
 #include "Player/STUPlayerController.h"
 #include "UI/STUGameHUD.h"
@@ -10,4 +11,32 @@ ASTUGameModeBase::ASTUGameModeBase() {
 	DefaultPawnClass = ASTUBaseCharacter::StaticClass();
 	PlayerControllerClass = ASTUPlayerController::StaticClass();
 	HUDClass = ASTUGameHUD::StaticClass();
+}
+
+void ASTUGameModeBase::StartPlay() {
+	SpawnBots();
+
+	Super::StartPlay();
+}
+
+UClass* ASTUGameModeBase::GetDefaultPawnClassForController_Implementation(AController* InController) {
+	if (InController && InController->IsPlayerController()) {
+		return Super::GetDefaultPawnClassForController_Implementation(InController);
+	}
+	return AIPawnClass;
+}
+
+void ASTUGameModeBase::SpawnBots() {
+	auto* World = GetWorld();
+	if (!World) {
+		return;
+	}
+
+	for (auto i = 0; i + 1 < Data.PlayersAmount; ++i) {
+		FActorSpawnParameters SpawnInfo;
+		SpawnInfo.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AdjustIfPossibleButAlwaysSpawn;
+
+		auto* Controller = World->SpawnActor<AAIController>(AIControllerClass, SpawnInfo);
+		RestartPlayer(Controller);
+	}
 }
--- Source/ShootThemUp/Public/STUGameModeBase.h
+++ Source/ShootThemUp/Public/STUGameModeBase.h
@@ -6,9 +6,16 @@
 #include "GameFramework/GameModeBase.h"
 #include "STUGameModeBase.generated.h"
 
-/**
- * 
- */
+class AAIController;
+
+USTRUCT(BlueprintType)
+struct FGameData {
+	GENERATED_BODY()
+
+	UPROPERTY(EditDefaultsOnly, meta = (ClampMin = "1", ClampMax = "100"))
+	int32 PlayersAmount = 2;
+};
+
 UCLASS()
 class SHOOTTHEMUP_API ASTUGameModeBase : public AGameModeBase
 {
@@ -16,4 +23,20 @@ class SHOOTTHEMUP_API ASTUGameModeBase : public AGameModeBase
 
 public:
 	ASTUGameModeBase();
+
+	virtual void StartPlay() override;
+	virtual UClass* GetDefaultPawnClassForController_Implementation(AController* InController) override;
+
+protected:
+	UPROPERTY(EditDefaultsOnly, Category = "Game")
+	TSubclassOf<AAIController> AIControllerClass;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Game")
+	TSubclassOf<APawn> AIPawnClass;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Game")
+	FGameData Data;
+
+private:
+	void SpawnBots();
 };
